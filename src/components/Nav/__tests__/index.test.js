6c0563a3c6d7f2e5cdb063753882de7ae45aa49c
kotaewing <kota.ewing@gmail.com>
2021-07-21 15:16:04 -0600

    wrote tests for gallery and photolist

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,18 +3,31 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..'
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
     // baseline test
     it('renders', () => {
-        render(<Nav></Nav>);
+        render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
     });
 
     // snapshot test
     it('matches snapshot', () => {
-        const { asFragment } = render(<Nav></Nav>);
-
+        const { asFragment } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
         expect(asFragment()).toMatchSnapshot();
     })
 })
@@ -22,7 +35,11 @@ describe('Nav component', () => {
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
         //arrange
-        const { getByLabelText } = render(<Nav></Nav>);
+        const { getByLabelText } = render(<Nav
+            categories={categories}
+            mockSetCurrentCategory={mockCurrentCategory}
+            currentCategory={mockSetCurrentCategory}
+        />);
 
         //asserta
         expect(getByLabelText('camera')).toHaveTextContent('📸')
@@ -31,7 +48,11 @@ describe('emoji is visible', () => {
 
 describe('links are visible', () => {
     it('inserts text into the links', () => {
-        const { getByTestId } = render(<Nav></Nav>);
+        const { getByTestId } = render(<Nav
+            categories={categories}
+            mockSetCurrentCategory={mockCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
 
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
         expect(getByTestId('about')).toHaveTextContent('About me');
